7432288e032400c53a3efcd85074d608966cea34
Jacky Rusly <jackyrusly@gmail.com>
2018-04-18 10:24:20 +0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -10,6 +11,8 @@ mongoose.connect('mongodb://jr:jrpassword@ds153494.mlab.com:53494/jr');
 mongoose.connection.once('open', () => {
     console.log('Connected to mLab');
 });
+
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
     schema,
